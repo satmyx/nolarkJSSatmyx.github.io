ae464f1d7caa2793e55564e3c8787a308031210a
Satmyx <dylanesculier@gmail.com>
2022-09-13 09:48:51 +0200

    Mise en place JS / HTML
    :+1:  : Ajout du front full html.
    :+1:  : Ajout de la base JS.

---
+++ js/remuneration.js
@@ -0,0 +1,18 @@
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/ClientSide/javascript.js to edit this template
+ */
+
+const fixe = 1100;
+
+function simulateur(){
+    let nbAncien = parseInt(window.document.querySelector(#i_anciennete).value);
+    let nbS20 = parseInt(window.document.querySelector(#i_nbs20).value);
+    let nbXSpirit = parseInt(window.document.querySelector(#i_nbxspirit).value);
+    let nbMultitec = parseInt(window.document.querySelector(#i_multitec).value);
+    
+}
+
+window.addEventListener("load", function () {
+    window.document.querySelector("#btn_envoyer_simu").addEventListener("click", simulateur);
+});
