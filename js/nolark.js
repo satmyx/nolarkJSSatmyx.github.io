48360e3623b713c8586ce593eaaebfb5d94e96ef
Satmyx <dylanesculier@gmail.com>
2022-09-13 09:17:50 +0200

    Ajout du fichier nolark.js
    Implémentation d'un test si le formulaire a eu le nom ou prénom sinon alert.

---
+++ js/nolark.js
@@ -0,0 +1,16 @@
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/ClientSide/javascript.js to edit this template
+ */
+
+function validEnvoi() {
+    if (window.document.querySelector("#i_nom").value === "" &&
+            window.document.querySelector("#i_prenom").value === "") {
+        alert("Le nom ou le prénom doivent être remplis"); // On affiche un message
+    } else {
+        window.document.querySelector("#form_contact").submit(); // On peut envoyer
+    }
+}
+window.addEventListener("load", function () {
+    window.document.querySelector("#btn_envoyer").addEventListener("click", validEnvoi);
+});
