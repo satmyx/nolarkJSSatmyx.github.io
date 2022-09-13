d8ee9c08fc695e6127775e780315495f59419be7
Satmyx <dylanesculier@gmail.com>
2022-09-13 11:44:51 +0200

    JS fait juste une correction nécessaire sur multitect

--- js/remuneration.js
+++ js/remuneration.js
@@ -6,22 +6,39 @@
 function Simulateur(){
     let nbAncien = parseInt(window.document.querySelector("#i_anciennete").value);
     let nbS20 = parseInt(window.document.querySelector("#i_nbs20").value);
+    let nbXSpirit = parseInt(window.document.querySelector("#i_nbxspirit").value);
+    let nbMultitec = parseInt(window.document.querySelector("#i_nbmultitec").value);
     let total = new Number();
-    let total2 = new Number();
     const fixe = 1100;
     const S20 = 140;
-    const XSpirit = 350;
+    const xspirit = 350;
     const Multitec = 180;
-    if (nbAncien >= 5)
+    if (nbAncien >= 10)
     {
-        total += fixe * 1.03;
-    }
-    else if (nbAncien > 10){
         total += fixe * 1.06;
     }
-    total2 = S20*4*0.02;
+    else if (nbAncien >= 5){
+        total += fixe * 1.03;
+    }
+    else {
+        total += fixe;
+    }
+    total = total + S20*nbS20*0.02;
+    if (nbXSpirit > 50)
+    {
+        total += xspirit*0.06;
+    }
+    if(nbMultitec >= 51){
+        total = total + Multitec*nbMultitec*0.10;
+    }
+    else if(nbMultitec >= 21){
+        total = total + Multitec*nbMultitec*0.06;
+    }
+    else if (nbMultitec <= 20){
+        total = total + Multitec*nbMultitec*0.04;
+    }
+    
     alert(total);
-    alert(total2);
 }
 
 window.addEventListener("load", function () {
