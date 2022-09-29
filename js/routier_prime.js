540e1809ab5c65ed246b7d08cc48c550ac052c5a
Satmyx <dylanesculier@gmail.com>
2022-09-29 11:23:25 +0200

    Ajout de la gestion des accidents
    :+1: : Ajout de la gestion des accidents.
    :+1:  : Rename du fichier HTML

--- js/routier_prime.js
+++ js/routier_prime.js
@@ -5,13 +5,23 @@
 
 function Simulateur(){
     let total = new Number();
-    total = get_prime_distance(nbKmParcouru) + get_prime_anciennete(nbAncien);
+    let nbAccident = parseInt(window.document.querySelector("#i_nbaccident").value);
+    if (nbAccident > 3){
+        total = 0;
+    }
+    else if (nbAccident === 0){
+        total = get_prime_distance() + get_prime_anciennete();
+    }
+    else{
+        total = (get_prime_distance() + get_prime_anciennete()) / get_nb_accident();
+    }
     window.document.querySelector("#resultat_remun").innerHTML = total + " €";
 }
 
-function get_prime_distance(nbKmParcouru){
+function get_prime_distance(){
     let total = new Number();
     let totalprime = new Number();
+    let nbKmParcouru = parseInt(window.document.querySelector("#i_nbkmparcouru").value);
     const primeDistance = 0.01;
     const plafondKmParcouru = 900;
     total += nbKmParcouru*primeDistance;
@@ -23,18 +33,33 @@ function get_prime_distance(nbKmParcouru){
     }
 }
 
-function get_prime_anciennete(nbAncien){
+function get_prime_anciennete(){
     let total = new Number();
     const primeAncienneteFixe = 300;
     const primeAjouterAnnee = 30;
+    const retirerAnneeDebut = 120;
+    let nbAncien = parseInt(window.document.querySelector("#i_anciennete").value);
     if(nbAncien > 4){
-        return total += primeAncienneteFixe + nbAncien*primeAjouterAnnee - 120;
+        return total += primeAncienneteFixe + nbAncien*primeAjouterAnnee - retirerAnneeDebut;
     }
     else if (nbAncien === 4){
         return total = primeAncienneteFixe;
     }
     else{
         return total += 0;
+    }
+}
+
+function get_nb_accident(){
+    let nbAccident = parseInt(window.document.querySelector("#i_nbaccident").value);
+    if (nbAccident === 1){
+        return 2;
+    }
+    else if (nbAccident === 2){
+        return 3;
+    }
+    else if (nbAccident === 3){
+        return 4;
     }
 }
 
